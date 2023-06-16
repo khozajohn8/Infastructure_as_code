8bf7a462ee05748ba1257d4b878e8f0f3d4f7c3c
khozajohn8 <khozajohn8@gmail.com>
2023-06-17 00:49:40 +0200

    added:
    -alb security group
    -ec2 security group

---
+++ modules/security_groups/main.tf
@@ -0,0 +1,66 @@
+
+# create security group for the alb
+resource "aws_security_group" "alb_security_group" {
+  name        = "alb security group"
+  description = "Allow ssh and http inbound traffic"
+
+  ingress {
+    description = "https access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb security group"
+  }
+}
+
+# create security group for EC2
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2 security group"
+  description = "enable http/https access on port 80/443 via alb sg"
+
+  ingress {
+    description      = "http access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.alb_security_group.id]
+  }
+
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.alb_security_group.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "ec2 security group"
+  }
+}
