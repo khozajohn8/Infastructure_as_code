9962e4e7f48d8129ad965bb039a78a92bcd91b91
khozajohn8 <khozajohn8@gmail.com>
2023-07-13 23:30:41 +0200

    deleted lock file & commented out RDS

--- my_website/main.tf
+++ my_website/main.tf
@@ -79,11 +79,11 @@ module "acm" {
   alternative_name              = var.alternative_name
 }
 
-module "rds" {
+/*module "rds" {
   source                        = "../modules/rds-instance"
   database_subnet_az1_id        = module.vpc.database_subnet_az1_id
   db_security_group_id          = module.security_group.db_security_group_id
-}
+}*/
 
 module "route_53" {
   source                             = "../modules/route53"
