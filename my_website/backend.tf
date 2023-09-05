0e6d5a90a1047d0d34182342fc5c339995885f07
khozajohn8 <khozajohn8@gmail.com>
2023-09-05 17:26:11 +0200

    adding files

--- my_website/backend.tf
+++ my_website/backend.tf
@@ -1,10 +1,10 @@
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = "my-website-terraform-remote-state-jk"
-    key       = "build/terraform.tfstate"
+    bucket    = "iac-jenkins-tfstate"
+    key       = "terraform.tfstate"
     region    = "us-east-1"
     profile = "Codebuild-user"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "iac-jenkins-tfstate-db-table"
   }
 }
